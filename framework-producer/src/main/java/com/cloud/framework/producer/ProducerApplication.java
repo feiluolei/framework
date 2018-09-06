411bce5a10bf733e3d7efec38ee3ae9b73403c82
linhanlin <438788927@qq.com>
2018-09-06 17:50:22 +0800

    init producer

---
+++ framework-producer/src/main/java/com/cloud/framework/producer/ProducerApplication.java
@@ -0,0 +1,15 @@
+package com.cloud.framework.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class ProducerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+}	
